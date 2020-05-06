29d97b48996ee31c9b15d1a1f4a78966ee5d65e4
Cosima Peter <cs.manughian@gmail.com>
2020-05-05 19:36:49 -0700

    Integrated favicon, dot-env, and plotly prototype to sentiment

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useRef } from 'react';
+import Plot from 'react-plotly.js';
+import axios from 'axios';
 import autosize from 'autosize';
 import './Dashboard.scss';
+import { useState } from 'react';
 
 function Dashboard() {
   const textEl = useRef(null);
@@ -15,15 +18,59 @@ function Dashboard() {
     autosize(textEl.current);
   }
 
+  let [textStream, setTextStream] = useState('');
+  let [analysis, setAnalysis] = useState(null);
+
+  function performAnalysis() {
+
+    axios.get(`${process.env.REACT_APP_TEXT_ANALYTICS_API}/sentiment/${textStream}`)
+      .then(function (response) {
+        setAnalysis(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+  }
+
   return (
     <div className='dashboard'>
       <WelcomeMessage />
+      {
+        analysis &&
+          <Plot
+            style={{
+              color: 'blue',
+              'margin': 'auto',
+              // 'font-family': 'rubik, -apple-system, sans-serif',
+            }}
+            data={[
+              {
+                type: 'bar', 
+                orientation: 'h',
+                x: Object.values(analysis),
+                y: ['neg', 'neu', 'pos', 'compound'],
+                marker: { color: '#fcca44' },
+              },
+            ]}
+            layout={{
+              width: 320, 
+              height: 240,
+              title: 'Sentiment'
+            }}
+            config={{
+              staticPlot: true,
+              showLegend: false
+            }}
+          />
+      }
       <textarea 
         ref={textEl}
+        defaultValue={textStream}
+        onChange={(e) => setTextStream(e.target.value)}
         placeholder='Stream of consciousness...' 
-        maxLength={2000}>
-      </textarea>
-      <button>done</button>
+        maxLength={2000} />
+      <button onClick={performAnalysis}>done</button>
     </div>
   )
 }
